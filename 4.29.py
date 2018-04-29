94d826468c99108968dff8588984ec724da3fb90
leotangjc <tangjiachengemail@gmail.com>
2018-04-29 14:30:04 +0800

    push

--- 4.29.py
+++ 4.29.py
@@ -10,7 +10,7 @@
 # print(0xff == 255)
 
 
-#请用两种方式打印出 hello, python.
+# 请用两种方式打印出 hello, python.
 # print('hello, python')
 # print('hello'', ''python')
 
@@ -27,8 +27,8 @@
 
 # 请把下面的字符串用r'''...'''的形式改写，并用print打印出来：
 # '\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
-print r'''"To be, or not to be": that is the question.
-Whether it's nobler in the mind to suffer.'''
+# print r'''"To be, or not to be": that is the question.
+# Whether it's nobler in the mind to suffer.'''
 
 
 # 在文件第一行添加注释  # -*- coding: utf-8 -*- 
