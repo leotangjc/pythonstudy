45afd94205494090d82fad1ead1b2a84d0658271
leotangjc <tangjiachengemail@gmail.com>
2018-04-29 14:29:25 +0800

    push

---
+++ 4.29.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#计算十进制整数 45678 和十六进制整数 0x12fd2 之和。
+# print(45678 + 0x12fd2)
+#请用字符串表示出Learn Python in imooc。
+# print('Learn Python in imooc')
+# 请计算以下表达式的布尔值（注意 == 表示判断是否相等）：
+# 00 < 99
+# 0xff == 255
+# print(00 < 99)
+# print(0xff == 255)
+
+
+#请用两种方式打印出 hello, python.
+# print('hello, python')
+# print('hello'', ''python')
+
+# 等差数列可以定义为每一项与它的前一项的差等于一个常数，可以用变量 x1 表示等差数列的第一项，用 d 表示公差，请计算数列
+# 1 4 7 10 13 16 19 ...
+# 前 100 项的和。
+# a=1
+# while a > 100:
+#     a+2
+#     if a>100:
+#           break
+# print(a)
+
+
+# 请把下面的字符串用r'''...'''的形式改写，并用print打印出来：
+# '\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
+print r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
+
+
+# 在文件第一行添加注释  # -*- coding: utf-8 -*- 
+# 可让解释器知道本文档使用utf-8编码
