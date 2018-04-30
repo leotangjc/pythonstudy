0b4858e7a58fa1a16adb6efa960d18cb04c107a4
LEOME-PC\leome <dubeduleo@gmail.com>
2018-04-30 15:02:42 +0800

    1

--- 4.30.py
+++ 4.30.py
@@ -54,3 +54,9 @@
 # 请修改代码，确保 t 是一个tuple。
 # t = ('Adam',)
 # print(t)
+
+# 定义了tuple：
+# t = ('a', 'b', ['A', 'B'])
+# 由于 t 包含一个list元素，导致tuple的内容是可变的。能否修改上述代码，让tuple内容不可变？
+t = ('a', 'b', 'A', 'B')
+print(t)
