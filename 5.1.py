2432012ca65ddfa3829b23175cfb93b73887ad83
leotangjc <tangjiachengemail@gmail.com>
2018-05-01 18:20:43 +0800

    1

---
+++ 5.1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# sum()函数接受一个list作为参数，并返回list所有元素之和。请计算 1*1 + 2*2 + 3*3 + ... + 100*100。
+# a=1
+# x=[]
+# while a<100:
+#     x.append(a*a)
+#     a=a+1
+# print(sum(x))
+
+
+# 请定义一个 square_of_sum 函数，它接受一个list，返回list中每个元素平方的和。
+# def square_of_sum(l):
+#     a=0
+#     for b in l:
+#         a=a+b*b
+#     return a
+# print(square_of_sum([1, 2, 3, 4, 5]))
+# print(square_of_sum([-5, 0, 5, 15, 25]))
+        
+# 请编写接受可变参数的 average() 函数。
+# def average(*a)
+#     a+
