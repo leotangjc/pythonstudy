f708788d3972c430f6be3fcab62c4b05623201df
LEOME-PC\leome <tangjiachengemail@gmail.com>
2018-05-15 23:39:55 +0800

    1

--- json/c1.py
+++ json/c1.py
@@ -39,12 +39,24 @@ a = 'abc|bac|cba|adc123callbsdiiikkkxy11we1cvb'
 # print(r)
 
 
-s = 'A8B20C4M5289R4'
-def convert(value):
-    matched = value.group()
-    if int(matched) >= 6:
-        return '9'
-    else:
-        return '0'
-r = re.sub('\d', convert, s)
-print(r)
+# s = 'A8B20C4M5289R4'
+# def convert(value):
+#     matched = value.group()
+#     if int(matched) >= 6:
+#         return '9'
+#     else:
+#         return '0'
+# r = re.sub('\d', convert, s)
+# print(r)
+
+
+
+import json
+
+json_str = '{"name":"qiyue","age":18}'
+
+student = json.loads(json_str)
+print(type(student))
+print(student)
+print(student['name'])
+print(student['age'])
