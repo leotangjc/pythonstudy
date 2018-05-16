0b7f0d20cec7ff4cc7099284c3620adfa78efde5
leotangjc <tangjiachengemail@gmail.com>
2018-05-16 17:03:59 +0800

    1

--- 5.16.py
+++ 5.16.py
@@ -2,20 +2,26 @@
 
 
 
-' a test module '
+# ' a test module '
 
-__author__ = 'Michael Liao'
+# __author__ = 'Michael Liao'
 
-import sys
+# import sys
 
-def test():
-    args = sys.argv
-    if len(args)==1:
-        print('Hello, world!')
-    elif len(args)==2:
-        print('Hello, %s!' % args[1])
-    else:
-        print('Too many arguments!')
+# def test():
+#     args = sys.argv
+#     if len(args)==1:
+#         print('Hello, world!')
+#     elif len(args)==2:
+#         print('Hello, %s!' % args[1])
+#     else:
+#         print('Too many arguments!')
 
-if __name__=='__main__':
-    test()
+# if __name__=='__main__':
+#     test()
+
+class Student(object):
+    pass
+
+bart = Student()
+print(bart)
