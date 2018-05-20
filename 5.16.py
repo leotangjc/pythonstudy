fb2bd3082ac8e4b01190a74ef4540ccbdb810515
leotangjc <tangjiachengemail@gmail.com>
2018-05-20 13:00:56 +0800

    1

--- 5.16.py
+++ 5.16.py
@@ -129,3 +129,34 @@ print(person('tjc', 21, city='shiyan'))
 
 
 # 命名关键字参数
+
+
+
+# 练习
+# 以下函数允许计算两个数的乘积，请稍加改造，变成可接收一个或多个数并计算乘积：
+def product(x, *y):
+    s = 1
+    s *= x
+    for z in y:
+        s *= z
+    return s
+
+
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
