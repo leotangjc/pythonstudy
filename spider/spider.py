9a470da2a1ef26c2312c8a7af142330f120bf07d
LEOME-PC\leome <tangjiachengemail@gmail.com>
2018-05-31 00:12:30 +0800

    1

--- spider/spider.py
+++ spider/spider.py
@@ -1,29 +1,41 @@
 # -*- coding: utf-8 -*-
+import re
 
 from urllib import request
 
 class Spider():
     url = 'https://www.panda.tv/cate/fortnite?pdt=1.27.psbar-re.2.555hmqlul05'
-    root_parrern = '<div class="video-info">[\s\S]*?</div>'
+    root_parrern = '<div class="video-info">([\s\S]*?)</div>'
+    name_pattern = '</i>([\s\S]*?)</span>'
+    number_pattern = '<span class="video-number">([\s\S]*?</span>)'
     
     def __fetch_content(self):
-        r = request.urlopen(Spider.url)
+        r = request.urlopen(Spider.url) # 使用方法读取指定页面，并将其生成一个对象
         htmls = r.read()
-        htmls = str(htmls,encoding='utf-8')
+        htmls = str(htmls,encoding='utf-8') #将读取到的网页编码转为utf-8格式
         return htmls
         
         
 
     def __analysis(self, htmls):
         root_html = re.findall(Spider.root_parrern, htmls)
-        print(root_html[0])
+        anchors = []
+        for html in root_html:
+            name = re.findall(Spider.name_pattern, html)
+            number = re.findall(Spider.number_pattern, html)
+            anchor = {'name':name, 'number':number}
+            anchors.append(anchor)
+        # print(anchors)
         a = 1
 
+    def __refine(self, anchors):
+        l = lambda anchor :{'name':anchor['name'][0].strip()}
 
-    def go(self):
+    def go(self):  #入口方法
         htmls = self.__fetch_content()
-        self.__analysis(htmls)
+        anchors = self.__analysis(htmls)
+        self.__refine(anchors)
 
 
-spider = Spider()
-spider.go()
+spider = Spider() #实例化
+spider.go() #调用入口方法
