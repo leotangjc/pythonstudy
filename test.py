97d80d38a218fea6b269e2be16a2a4dde0aff5a2
tangJC <leo@tangJCdeMBP.lan>
2018-06-18 14:24:41 +0800

    1

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
-try:
-    print('try...')
-    r = 10 / 0
-    print('result:', r)
-except ZeroDivisionError as e:
-    print('except:', e)
-finally:
-    print('finally...')
-print('END')
+# try:
+#     print('try...')
+#     r = 10 / 0
+#     print('result:', r)
+# except ZeroDivisionError as e:
+#     print('except:', e)
+# finally:
+#     print('finally...')
+# print('END')
+
+
+def dan(a, b):
+    if a%2 == 1:
+        while a < b:
+            a += 2
+            print(a)
+    else:
+        while a < b:
+            a += 2
+            print(a)
+
+print(dan(2, 14))
