33f7d6697e78fb26f4c5ac58616997a34dea0654
leotangjc-MagicBook <tangjiachengemail@gmail.com>
2020-03-18 23:26:13 +0800

    #20.3.18

--- Archive/4.29.py
+++ Archive/4.29.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+
+
 #计算十进制整数 45678 和十六进制整数 0x12fd2 之和。
+
 # print(45678 + 0x12fd2)
 #请用字符串表示出Learn Python in imooc。
 # print('Learn Python in imooc')
+
 # 请计算以下表达式的布尔值（注意 == 表示判断是否相等）：
 # 00 < 99
 # 0xff == 255
@@ -27,8 +31,8 @@
 
 # 请把下面的字符串用r'''...'''的形式改写，并用print打印出来：
 # '\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
-# print r'''"To be, or not to be": that is the question.
-# Whether it's nobler in the mind to suffer.'''
+# print(r'''"To be, or not to be": that is the question.
+# Whether it's nobler in the mind to suffer.''')
 
 
 # 在文件第一行添加注释  # -*- coding: utf-8 -*- 
@@ -40,8 +44,8 @@
 
 
 # a = 'python'
-# print 'hello,', a or 'world'
+# print('hello,', a or 'world')
 # b = ''
-# print 'hello,', b or 'world'
-# 因为在做布尔运算时，计算的第一个元素结果就决定了这个运算的结果
+# print('hello,', b or 'world')
+# # 因为在做布尔运算时，计算的第一个元素结果就决定了这个运算的结果
 
