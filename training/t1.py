4969cc601df3b0931edbf6497689dffea338a73a
leotangjc-MagicBook <tangjiachengemail@gmail.com>
2020-06-03 15:49:23 +0800

    1

--- training/t1.py
+++ training/t1.py
@@ -271,13 +271,15 @@
 
 
 
-alien_0 = {'x_position':0, 'y_position':25, 'speed':'fast'}
-print("Original x_position: " + str(alien_0['x_position']))
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    x_increment = 3
-alien_0['x_positon'] = alien_0['x_position'] + x_increment
-print("new x_position: " + str(alien_0['x_position']))
+# alien_0 = {'x_position':0, 'y_position':25, 'speed':'fast'}
+# print("Original x_position: " + str(alien_0['x_position']))
+# if alien_0['speed'] == 'slow':
+#     x_increment = 1
+# elif alien_0['speed'] == 'medium':
+#     x_increment = 2
+# else:
+#     x_increment = 3
+# alien_0['x_positon'] = alien_0['x_position'] + x_increment
+# print("new x_position: " + str(alien_0['x_position']))
+
+print("aaa\naaa","ccc")
