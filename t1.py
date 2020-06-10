bd4cbd2bd35924b484b98aa2e1f310b0001b861a
leotangjc <tangjiachengemail@gmail.com>
2020-06-10 19:50:49 +0800

    1

---
+++ t1.py
@@ -0,0 +1,26 @@
+# a = 1
+# b = 2
+# if a != b:
+#     print("True")
+# else:
+#     print("False")
+
+
+# alen_color = ["green","yellow","red"]
+# if "green" in alien_color:
+#     print("you got five points")
+# elif "yellow" in alien_color:
+#     print("you got ten points")
+# else:
+#     print("you got fifteen point")
+
+# a = input("输入你的颜色")
+a = "g"
+if a == "g":
+    print("ig")
+elif a == "r":
+    print("ir")
+elif a == "y":
+    print("iy")
+else:
+    pass
